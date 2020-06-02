39c7d3b05c55a56faf3244733d37fe54dab5c853
prachi-shrma <prachi-shrma@users.noreply.github.com>
2020-06-03 00:36:42 +0530

    Code file created.

---
+++ project-on-class-record/code.py
@@ -0,0 +1,68 @@
+# --------------
+# Code starts here
+
+# Create the lists 
+class_1=["Geoffrey Hinton","Andrew Ng","Sebastian Raschka","Yoshua Bengio"]
+class_2=["Hilary Mason","Carla Gentry","Corinna Cortes"]
+# Concatenate both the strings
+new_class= class_1+class_2
+
+# Append the list
+new_class.append("Peter Warden")
+
+# Print updated list
+print(new_class)
+
+# Remove the element from the list
+new_class.remove("Carla Gentry")
+# Print the list
+print(new_class)
+
+
+# Create the Dictionary
+courses={"Math":65,"English":70,"History":80,"French":70,"Science":60}
+total=65+70+80+70+60
+print(total)
+percentage=total/5
+print(percentage)
+
+
+# Slice the dict and stores  the all subjects marks in variable
+
+
+# Store the all the subject in one variable `Total`
+
+# Print the total
+
+# Insert percentage formula
+
+# Print the percentage
+
+
+
+
+# Create the Dictionary
+mathematics={"Geoffrey Hinton":78,"Andrew Ng":95,"Sebastian Raschka":65,"Yoshua Benjio":50,"Hilary Mason":70,"Corinna Cortes":66,"Peter warden":75}
+topper=max(mathematics,key=mathematics.get)
+print(topper)
+
+
+
+
+# Given string
+first_name= topper.split()[1]
+
+# Create variable first_name 
+Last_name= topper.split()[0]
+# Create variable Last_name and store last two element in the list
+
+# Concatenate the string
+full_name=first_name+' '+ Last_name
+certificate_name=full_name.upper()
+# print the full_name
+print(certificate_name)
+# print the name in upper case
+
+# Code ends here
+
+
